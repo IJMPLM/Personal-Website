260fa0091e62be5c68deee3d418d5463039e5eaf
IJMPLM <iwagjohnmichael@gmail.com>
2023-12-27 21:55:19 +0800

    all from pc

---
+++ index.js
@@ -0,0 +1,21 @@
+// var typed = document.querySelector(".typed");
+    
+// // Define an array of words to cycle through
+// var words = ["World", "CSS", "Animation", "JavaScript"];
+
+// // Define a variable to keep track of the current index
+// var index = 0;
+
+// // Add an event listener for the animationend event
+// typed.addEventListener("animationend", function() {
+//   // Increment the index by one
+//   index++;
+  
+//   // If the index is equal to the length of the array, reset it to zero
+//   if (index == words.length) {
+//     index = 0;
+//   }
+  
+//   // Change the text content of the div element to the next word
+//   typed.textContent = words[index];
+// });
